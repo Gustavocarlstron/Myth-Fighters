0b8a2919ca2b94d0c339fe192bca1d7b5f017827
GustavoCarlstron <gustavo.carlstron@gmail.com>
2019-04-17 15:58:21 -0300

    Escrevendo na tela com a fonte e realizando testes de clic do mouse para realizar o click foi necessário criar um retangulo da surface e posicionado na onde esta o escrito

--- menus.py
+++ menus.py
@@ -1,7 +1,30 @@
 import pygame
 
+
 def Abertura(tela):
-    tela.fill((0, 0, 0))
-#def Menu():
+    for i in range(255):
+        tela.fill((i, i, i))
+        pygame.display.update()
+    return 1
+
+def Menu(tela):
+    while True:
+        font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 30)
+        teste = font.render("TESTE", True, (100, 100,100))
+        tela.blit(teste, (170, 400))
+        testeRect = teste.get_rect()
+        testeRect.x = 170
+        testeRect.y = 400
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 9
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if testeRect.collidepoint(pygame.mouse.get_pos()):
+                    print("Deu Certo")
+                else:
+                    print("Clicou errado")
+        pygame.display.update()
+
+
 #def Configuração():
 #def Credito():
