2ee9108d648256d0371e500431ab69c2a0445882
GustavoCarlstron <gustavo.carlstron@gmail.com>
2019-04-17 16:14:04 -0300

    Só zuando, agora quando clica o teste muda de lugar. :)

--- menus.py
+++ menus.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 def Abertura(tela):
@@ -8,21 +9,28 @@ def Abertura(tela):
     return 1
 
 def Menu(tela):
+    font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 30)
+    teste = font.render("TESTE", True, (100, 100, 100))
+
+    testeRect = teste.get_rect()
+    testex = random.randint(0, 800)
+    testey = random.randint(0, 600)
+    testeRect.x = testex
+    testeRect.y = testey
+
     while True:
-        font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 30)
-        teste = font.render("TESTE", True, (100, 100,100))
-        tela.blit(teste, (170, 400))
-        testeRect = teste.get_rect()
-        testeRect.x = 170
-        testeRect.y = 400
+        tela.fill((255, 255, 255))
+        tela.blit(teste, (testex, testey))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 9
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if testeRect.collidepoint(pygame.mouse.get_pos()):
-                    print("Deu Certo")
-                else:
-                    print("Clicou errado")
+                    testex = random.randint(0, 800)
+                    testey = random.randint(0, 600)
+                    testeRect.x = testex
+                    testeRect.y = testey
         pygame.display.update()
 
 
