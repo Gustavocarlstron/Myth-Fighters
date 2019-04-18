f9543331ae5ae51c5fb5b92f2c2ef10d5a81dbfb
GustavoCarlstron <gustavo.carlstron@gmail.com>
2019-04-18 10:21:37 -0300

    Agora o texto almenta. precisa centralizar ainda

--- menus.py
+++ menus.py
@@ -25,12 +25,20 @@ def Menu(tela):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 9
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if testeRect.collidepoint(pygame.mouse.get_pos()):
+            if testeRect.collidepoint(pygame.mouse.get_pos()):
+                font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 40)
+                teste = font.render("TESTE", True, (100, 100, 100))
+                if event.type == pygame.MOUSEBUTTONDOWN:
                     testex = random.randint(0, 800)
                     testey = random.randint(0, 600)
                     testeRect.x = testex
                     testeRect.y = testey
+            else:
+                font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 30)
+                teste = font.render("TESTE", True, (100, 100, 100))
+
+        tela.fill((255, 255, 255))
+        tela.blit(teste, (testex, testey))
         pygame.display.update()
 
 
