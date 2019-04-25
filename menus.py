b8b9c511b19092ff1cc35b0afa9b00a277e115b2
Gustavo Carlstron <gustavo.carlstron@gmail.com>
2019-04-24 22:22:47 -0300

    Codificada a tela de menu com botões funcionais.

--- menus.py
+++ menus.py
@@ -22,38 +22,54 @@ def Abertura(tela):
 
 
 def Menu(tela):
-    font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 30)
-    teste = font.render("TESTE", True, (100, 100, 100))
-
-    testex = random.randint(0, 800)
-    testey = random.randint(0, 600)
-    testeRect = TextRect(teste, testex, testey)
+    btnIniciar = Botao("Botoes/botaoIniciar.png", "Botoes/botaoIniciarAtivo.png", 100, 300)
+    btnConfiguracao = Botao("Botoes/botaoConfiguracao.png", "Botoes/botaoConfiguracaoAtivo.png", 100, 350)
+    btnCredito = Botao("Botoes/botaoCredito.png", "Botoes/botaoCreditoAtivo.png", 100, 400)
+    btnSair = Botao("Botoes/botaoSair.png", "Botoes/botaoSairAtivo.png", 100, 450)
 
     while True:
-        tela.fill((255, 255, 255))
-        tela.blit(teste, (testex, testey))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 9
 
-            #Iniciar
-            if testeRect.collidepoint(pygame.mouse.get_pos()):
-                font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 40)
-                teste = font.render("TESTE", True, (100, 100, 100))
-
+            #Botão Iniciar
+            if btnIniciar.rect.collidepoint(pygame.mouse.get_pos()):
+                btnIniciar.img = btnIniciar.ativo
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    testex = random.randint(0, 800)
-                    testey = random.randint(0, 600)
-                    testeRect.x = testex
-                    testeRect.y = testey
-
+                    pass
             else:
-                font = pygame.font.Font('Fontes/TlwgTypist-Bold.ttf', 30)
-                teste = font.render("TESTE", True, (100, 100, 100))
+                btnIniciar.img = btnIniciar.inativo
+
+            #Botão Confiuração
+            if btnConfiguracao.rect.collidepoint(pygame.mouse.get_pos()):
+                btnConfiguracao.img = btnConfiguracao.ativo
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+            else:
+                btnConfiguracao.img = btnConfiguracao.inativo
+
+            #Botão Credito
+            if btnCredito.rect.collidepoint(pygame.mouse.get_pos()):
+                btnCredito.img = btnCredito.ativo
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+            else:
+                btnCredito.img = btnCredito.inativo
+
+            #Botão Sair
+            if btnSair.rect.collidepoint(pygame.mouse.get_pos()):
+                btnSair.img = btnSair.ativo
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+            else:
+                btnSair.img = btnSair.inativo
 
         tela.fill((255, 255, 255))
-        tela.blit(teste, (testex, testey))
+        tela.blit(btnIniciar.img, (btnIniciar.x, btnIniciar.y))
+        tela.blit(btnConfiguracao.img, (btnConfiguracao.x, btnConfiguracao.y))
+        tela.blit(btnCredito.img, (btnCredito.x, btnCredito.y))
+        tela.blit(btnSair.img, (btnSair.x, btnSair.y))
         pygame.display.update()
 
 
@@ -66,3 +82,19 @@ def TextRect(TextRender, x, y):
     textRect.x = x
     textRect.y = y
     return textRect
+
+class Botao(object):
+    img = 0
+    inativo = 0
+    ativo = 0
+    rect = 0
+    x = 0
+    y = 0
+
+    def __init__(self, inativo, ativo, x, y):
+        self.inativo = pygame.image.load(inativo)
+        self.ativo = pygame.image.load(ativo)
+        self.img = self.ativo
+        self.rect = pygame.Rect((x, y), self.ativo.get_size())
+        self.x = x
+        self.y = y
