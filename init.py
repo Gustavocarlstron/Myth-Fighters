df8f741505f4e34cc48164e8de9f3aa52f2a4254
Gustavo Carlstron <gustavo.carlstron@gmail.com>
2019-04-27 15:08:54 -0300

    Configurado Selecinar personagem

--- init.py
+++ init.py
@@ -24,16 +24,13 @@ while True:
         estado = Menu(tela)
     elif estado == 2:
         estado = Configuração()
-        pass
     elif estado == 3:
         estado = Credito()
-        pass
     elif estado == 4:
-        print("Selecionar Personagem ainda não funciona, voltar ao menu")
-        estado = 1
-        estado = SelecionarPersonagem()
-        pass
+        estado = SelecionarPersonagem(tela)
     elif estado == 5:
+        print("SelecionarFase ainda não funciona, voltar ao menu")
+        estado = 1
         #estado = SelecionarFase()
         pass
     elif estado == 6:
