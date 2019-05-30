d9532b05a227522804865e031724412f0ede5e70
Gustavo Carlstron <gustavo.carlstron@gmail.com.br>
2019-05-30 17:17:35 -0300

    Agora os 2 players podem selecionar todos personagens

--- selecao.py
+++ selecao.py
@@ -1,7 +1,7 @@
 import pygame
 from Botao import Botao
 import controles as key
-selectP1 = 1
+selectP1 = 5
 selectP2 = 1
 
 
@@ -9,9 +9,11 @@ def SelecionarPersonagem(tela):
     global selectP1, selectP2
     Play1OK = True
     Play2OK = True
-    x, y = 0, 0
+    x1, y1 = 0, 0
+    x2, y2 = 0, 0
 
     seletorP1 = pygame.image.load("Imagens/RetratoPersonagens/select.png")
+    seletorP2 = pygame.image.load("Imagens/RetratoPersonagens/select2.png")
 
     personagem1 = Botao("Imagens/RetratoPersonagens/personagem1.png",
                         "Imagens/RetratoPersonagens/personagem1Ativo.png",
@@ -44,28 +46,40 @@ def SelecionarPersonagem(tela):
                 return 9
 
             if event.type == pygame.KEYDOWN:
-                if selectP1 >= 1 and selectP1 <= 4:
+                if selectP1 >= 1 and selectP1 <= 8:
                     if event.key == key.P1Cima:
                         selectP1 = selectP1 - 1
-                    elif event.key == key.P1Baixo:
+                    if event.key == key.P1Baixo:
                         selectP1 = selectP1 + 1
+                    if event.key == key.P1Esquerda:
+                        if not(selectP1 - 4 < 1):
+                            selectP1 = selectP1 - 4
+                    if event.key == key.P1Direita:
+                        if not(selectP1 + 4 > 8):
+                            selectP1 = selectP1 + 4
 
-                if selectP2 >= 1 and selectP2 <= 4:
+                if selectP2 >= 1 and selectP2 <= 8:
                     if event.key == key.P2Cima:
                         selectP2 = selectP2 - 1
                     if event.key == key.P2Baixo:
                         selectP2 = selectP2 + 1
+                    if event.key == key.P2Esquerda:
+                        if not(selectP2 - 4 < 1):
+                            selectP2 = selectP2 - 4
+                    if event.key == key.P2Direita:
+                        if not(selectP2 + 4 > 8):
+                            selectP2 = selectP2 + 4
 
 
         if selectP1 < 1:
             selectP1 = 1
-        elif selectP1 > 4:
-            selectP1 = 4
+        elif selectP1 > 8:
+            selectP1 = 8
 
         if selectP2 < 1:
             selectP2 = 1
-        elif selectP2 > 4:
-            selectP2 = 4
+        elif selectP2 > 8:
+            selectP2 = 8
 
         personagem1.Inativo()
         personagem2.Inativo()
@@ -78,28 +92,55 @@ def SelecionarPersonagem(tela):
 
         #Player1 Regras de seleção
         if selectP1 == 1:
-            x = personagem5.GetX()
-            y = personagem5.GetY()
+            x1 = personagem1.GetX()
+            y1 = personagem1.GetY()
         elif selectP1 == 2:
-            personagem6.Ativo()
-            x = personagem6.GetX()
-            y = personagem6.GetY()
+            x1 = personagem2.GetX()
+            y1 = personagem2.GetY()
         elif selectP1 == 3:
-            x = personagem7.GetX()
-            y = personagem7.GetY()
+            x1 = personagem3.GetX()
+            y1 = personagem3.GetY()
         elif selectP1 == 4:
-            x = personagem7.GetX()
-            y = personagem7.GetY()
+            x1 = personagem4.GetX()
+            y1 = personagem4.GetY()
+        elif selectP1 == 5:
+            x1 = personagem5.GetX()
+            y1 = personagem5.GetY()
+        elif selectP1 == 6:
+            x1 = personagem6.GetX()
+            y1 = personagem6.GetY()
+        elif selectP1 == 7:
+            x1 = personagem7.GetX()
+            y1 = personagem7.GetY()
+        elif selectP1 == 8:
+            x1 = personagem8.GetX()
+            y1 = personagem8.GetY()
 
         #Player2 Regras de seleção
         if selectP2 == 1:
-            personagem1.Ativo()
+            x2 = personagem1.GetX()
+            y2 = personagem1.GetY()
         elif selectP2 == 2:
-            personagem2.Ativo()
+            x2 = personagem2.GetX()
+            y2 = personagem2.GetY()
         elif selectP2 == 3:
-            personagem3.Ativo()
+            x2 = personagem3.GetX()
+            y2 = personagem3.GetY()
         elif selectP2 == 4:
-            personagem4.Ativo()
+            x2 = personagem4.GetX()
+            y2 = personagem4.GetY()
+        elif selectP2 == 5:
+            x2 = personagem5.GetX()
+            y2 = personagem5.GetY()
+        elif selectP2 == 6:
+            x2 = personagem6.GetX()
+            y2 = personagem6.GetY()
+        elif selectP2 == 7:
+            x2 = personagem7.GetX()
+            y2 = personagem7.GetY()
+        elif selectP2 == 8:
+            x2 = personagem8.GetX()
+            y2 = personagem8.GetY()
 
         tela.fill((255, 255, 255))
 
@@ -111,7 +152,8 @@ def SelecionarPersonagem(tela):
         tela.blit(personagem6.img, (personagem6.x, personagem6.y))
         tela.blit(personagem7.img, (personagem7.x, personagem7.y))
         tela.blit(personagem8.img, (personagem8.x, personagem8.y))
-        tela.blit(seletorP1, (x, y))
+        tela.blit(seletorP1, (x1, y1))
+        tela.blit(seletorP2, (x2, y2))
         pygame.display.update()
     return 5
 
