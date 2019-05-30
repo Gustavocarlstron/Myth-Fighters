8a5f9fa72bb53a51639c0209d508a4dd60126d8e
Gustavo Carlstron <gustavo.carlstron@gmail.com.br>
2019-05-30 16:31:51 -0300

    init só funciona se for a código principal. se for chamado por outros código não funciona mais

--- init.py
+++ init.py
@@ -6,44 +6,53 @@ telaLargura = 800
 telaAltura = 600
 estado = 0
 
+
+
 try:
     pygame.init()
 except:
     print("O modulo pygame não foi iniciado com sucesso")
 
+
 tela = pygame.display.set_mode((telaLargura, telaAltura), pygame.DOUBLEBUF, 32)
 fps = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            estado = 9
-    if estado == 0:
-        estado = Abertura(tela)
-    elif estado == 1:
-        estado = Menu(tela)
-    elif estado == 2:
-        estado = Configuracao()
-    elif estado == 3:
-        estado = Credito()
-    elif estado == 4:
-        estado = SelecionarPersonagem(tela)
-    elif estado == 5:
-        estado = 1
-        #estado = SelecionarFase()
-        pass
-    elif estado == 6:
-        #estado = Luta()
-        pass
-    elif estado == 7:
-        #estado = FimLuta()
-        pass
-    elif estado == 8:
-        #estado = Pause()
-        pass
-    elif estado == 9:
-        #Sair
-        break
-pygame.quit()
-exit()
+def init():
+    global estado, tela
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                estado = 9
+        if estado == 0:
+            estado = Abertura(tela)
+        elif estado == 1:
+            estado = Menu(tela)
+        elif estado == 2:
+            estado = Configuracao()
+        elif estado == 3:
+            estado = Credito()
+        elif estado == 4:
+            estado = SelecionarPersonagem(tela)
+        elif estado == 5:
+            estado = 1
+            #estado = SelecionarFase()
+            pass
+        elif estado == 6:
+            #estado = Luta()
+            pass
+        elif estado == 7:
+            #estado = FimLuta()
+            pass
+        elif estado == 8:
+            #estado = Pause()
+            pass
+        elif estado == 9:
+            Sair()
 
+def Sair():
+    pygame.quit()
+    exit()
+
+
+if __name__ == "__main__":
+    init()
